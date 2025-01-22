c7f10b22b09db77fc15a9028d2cf895cd503106c
Hrishi93 <sonumahure@gmail.com>
2025-01-22 11:07:51 +0530

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+//external JS
+
+function inner(){
+    document.getElementById('demo1').innerHTML="Welcome in FDP as External manupulation with Function"
+}
+
+function alertMsg(){
+    window.alert("Welcome in FDP as External manupulation with Function at Alert Box");
+}
+
+function consoleMsg(){
+    console.log("Welcome in FDP as External manupulation with Function at Console");
+}
+
+function writeMsg(){
+    document.write('Welcome in FDP as External manupulation with Function at document / changing DOM')
+}
